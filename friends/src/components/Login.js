b86b085c0b965edf755a2f7447299f86bf98ffde
Marissa <marissacooterl@gmail.com>
2020-04-21 15:43:48 -0500

    created login component and form

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -10,7 +10,47 @@ class Login extends React.Component {
     }
 
     handleChange = e => {
-        
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+    login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('/api/login', this.state.credentials)
+            .then(res => {
+                localStorage.setItem('token', JSON.stringify(res.data.payload));
+                this.props.history.push('/protected');
+            })
+            .catch(err => console.log({err}));
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.login}>
+                    <input
+                        type='text'
+                        name='username'
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type='text'
+                        name='password'
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Log In</button>    
+                </form>
+            </div>
+        )
     }
 
 }
+
+export default Login;
