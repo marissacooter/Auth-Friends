35f85c478f791f6671478907a2ff8f7fb5b77d7b
Marissa <marissacooterl@gmail.com>
2020-04-21 15:25:49 -0500

    checking pushes

---
+++ friends/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import FriendsList from "./components/FriendsList";
+import AddFriend from "./components/AddFriend";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+
+class Login extends React.Component {
+    state = {
+        credentials: {
+            username:'',
+            password:''
+        }
+    }
+
+    handleChange = e => {
+        
+    }
+
+}
